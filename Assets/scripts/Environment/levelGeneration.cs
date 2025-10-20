48e5e6e58bb07a2f84a9462542f076fb40b565cf
NonDrip <dseward@ucsc.edu>
2025-10-19 17:15:20 -0700

    Added script to lanes allowing for dynamic width.

--- Assets/scripts/Environment/levelGeneration.cs
+++ Assets/scripts/Environment/levelGeneration.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class levelGeneration : MonoBehaviour
 {
+    GameObject laneSprite;
+    public SpriteRenderer lanes;
+    public float width = 4;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        debug.Log("wow!");
+        lanes = GetComponentInChildren<SpriteRenderer>();
+        lanes.size = new Vector2(width * 0.1f, 0.5f);
+        //Debug.Log("Start!");
     }
 
     // Update is called once per frame
