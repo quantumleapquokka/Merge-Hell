15ff89eb765523c2b268417c1d986aac9cb4655f
Bhavya Anil <bhavya.anil2023@gmail.com>
2025-10-22 00:13:59 -0700

    car collision + game ending logic

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Car"))
+        {
+            Debug.Log("Game Over");
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
