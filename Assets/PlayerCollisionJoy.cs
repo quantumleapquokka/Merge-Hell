c0265ef86041faeff11b9631f618f79897c19836
NonDrip <dseward@ucsc.edu>
2025-10-23 00:20:15 -0700

    completed joyful branch

--- Assets/PlayerCollisionJoy.cs
+++ Assets/PlayerCollisionJoy.cs
@@ -1,12 +1,22 @@
+using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerCollisionJoy : MonoBehaviour
 {
+    UnityEngine.Object explosion;
+    void Start()
+    {
+       explosion = Resources.Load("Explosion");
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Car"))
         {
+            GameObject target = collision.gameObject;
+
+            UnityEngine.Object explosionInstance = Instantiate(explosion, target.transform.position, target.transform.rotation);
             Destroy(collision.gameObject);
         }
     }
--- Assets/Prefabs/Environment/ExplosionAnim.cs
+++ Assets/Prefabs/Environment/ExplosionAnim.cs
@@ -2,15 +2,20 @@ using UnityEngine;
 
 public class ExplosionAnim : MonoBehaviour
 {
+    ParticleSystem explosion;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        explosion = GetComponent<ParticleSystem>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (explosion.isStopped)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
